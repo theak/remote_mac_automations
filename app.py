29b64773b3ca34f1b8493e0815038e5fdbd886f9
Akshay Kannan <akshayk@google.com>
2023-02-24 13:33:05 -0800

    Added way more commands

--- app.py
+++ app.py
@@ -18,6 +18,36 @@ def playpause():
     subprocess.Popen(['osascript', SCRIPT_TMP % 'playpause'])
     return 'OK'
 
+@app.route("/left")
+def left():
+    subprocess.Popen(['osascript', SCRIPT_TMP % 'left'])
+    return 'OK'
+
+@app.route("/right")
+def right():
+    subprocess.Popen(['osascript', SCRIPT_TMP % 'right'])
+    return 'OK'
+
+@app.route("/up")
+def up():
+    subprocess.Popen(['osascript', SCRIPT_TMP % 'up'])
+    return 'OK'
+
+@app.route("/down")
+def down():
+    subprocess.Popen(['osascript', SCRIPT_TMP % 'down'])
+    return 'OK'
+
+@app.route("/open")
+def open():
+    subprocess.Popen(['osascript', SCRIPT_TMP % 'open'])
+    return 'OK'
+
+@app.route("/kodi")
+def kodi():
+    subprocess.Popen(['osascript', SCRIPT_TMP % 'kodi'])
+    return 'OK'
+
 @app.route("/next")
 def next():
     subprocess.Popen(['osascript', SCRIPT_TMP % 'next'])
